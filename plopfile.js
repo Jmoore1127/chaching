6fee1815bc4bdc73516f213a94e121219351c00e
Jared Moore <jmoore@chatterspot.com>
2017-10-27 18:53:06 -0600

    plop component

---
+++ plopfile.js
@@ -0,0 +1,15 @@
+module.exports = function (plop) {
+    plop.setGenerator('component', {
+        description: 'Create a react-native component',
+        prompts: [{
+            type: 'input',
+            name: 'name',
+            message: 'name'
+        }],
+        actions: [{
+            type: 'add',
+            path: 'src/ui/{{name}}/{{name}}.component.js',
+            templateFile: '.plop/component.hbs'
+        }]
+    });
+};
